3796086fe4829f8bb1dac1912ac3f68aeb5ef0f6
deliaspanigo <david.elias949@gmail.com>
2025-10-17 11:15:51 +0200

    Finalized version 1.0.2 of my R package: ready for launch.

--- inst/shiny/myApp/app.R
+++ inst/shiny/myApp/app.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(bslib)
 library(ggplot2)
+library("miniUI")
+library('rstudioapi')
 
 ui <- page_sidebar(
   sidebar = sidebar(
---
+++ script004_github_version.R
@@ -0,0 +1,15 @@
+# 0 - TOdos lso archivos
+git add .
+
+# 1. Perform the final commit for the release version.
+git commit -m "Finalized version 1.0.1 of my R package: ready for launch."
+
+# 2. Declare the annotated tag (using -a) to label the previous commit.
+# (Make sure to change 'v1.0.0' and the message).
+git tag -a v1.0.1 -m "Stable version 1.0.1 of the package."
+
+# 3. Push the commits from the current branch (e.g., 'main' or 'master') to the remote repository.
+git push
+
+# 4. Push the tags to the remote repository. This is the crucial step for the versioning!
+git push --tags
